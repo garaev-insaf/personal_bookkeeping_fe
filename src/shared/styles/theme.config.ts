b5e3bac96a9e0be8d7d1564e3cb4ed04f3726c55
scotch <igaraev@solit-clouds.ru>
2025-01-25 19:15:24 +0300

    feat: init and dev colors

---
+++ src/shared/styles/theme.config.ts
@@ -0,0 +1,29 @@
+import { ThemeConfig } from 'antd';
+
+export const themeConfig: ThemeConfig = {
+    // token: {
+    //     fontFamily: 'Inter',
+    //     colorPrimary: COLORS.primary,
+    //     colorPrimaryHover: COLORS.primaryHover,
+    //     colorError: COLORS.danger,
+    //     lineWidthFocus: 2,
+    // },
+    // components: {
+    //     Radio: radioConfig,
+    //     Segmented: switchToggleConfig,
+    //     Button: buttonConfig,
+    //     Input: inputConfig,
+    //     Select: selectConfig,
+    //     Tabs: tabsConfig,
+    //     Layout: layoutConfig,
+    //     Menu: menuConfig,
+    //     Switch: toggleConfig,
+    //     Pagination: paginationConfig,
+    //     Table: tableConfigDefault,
+    //     Modal: modalConfigDefault,
+    //     DatePicker: datePickerConfig,
+    //     Tree: treeConfig,
+    //     Collapse: collapseConfig,
+    // },
+    // hashed: false,
+};
